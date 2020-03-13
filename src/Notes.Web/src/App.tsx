84878fc02e287a94f9556c82f0a180df95ebb5da
David Reynolds <david.reynolds@zonal.co.uk>
2020-03-13 10:51:09 +0000

    Added auth0

--- src/Notes.Web/src/App.tsx
+++ src/Notes.Web/src/App.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useAuth0 } from "./utils/hooks";
+import Login from "./Common/Components/Login/Login"
 
 import { NotesPage } from './Notes/Components/NotesPage/NotesPage';
 import { NewNotePage } from './Notes/Components/NewNotePage/NewNotePage';
 
 function App() {
+
+  const { isAuthenticated, loading } = useAuth0();
+  
+  if (loading === undefined && isAuthenticated === undefined) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container fluid={true} className="p-3">
       <Router>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand>Notes</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to="/">Notes</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-          <Switch>
-            <Route exact path="/">
-              <NotesPage />
-            </Route>
-            <Route path="/notes/new">
-              <NewNotePage />
-            </Route>
-          </Switch>
+        <div>{isAuthenticated ? 
+        <div>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand>Notes</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link as={Link} to="/">Notes</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+            <Switch>
+              <Route exact path="/">
+                <NotesPage />
+              </Route>
+              <Route path="/notes/new">
+                <NewNotePage />
+              </Route>
+            </Switch>
+          </div>
+        : <Login />}</div>
       </Router>
     </Container>
   );
--- src/Notes.Web/src/index.tsx
+++ src/Notes.Web/src/index.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from "./utils/react-auth0-wrapper";
+import history from "./utils/history";
 
 import './App.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const onRedirectCallback = (appState?: any) => {
+    history.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  };
+
+ReactDOM.render(
+    <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        client_id={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        redirect_uri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+    >
+    <App />
+    </Auth0Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
