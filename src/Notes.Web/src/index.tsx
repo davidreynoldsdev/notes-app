02128a79ad230bab2955c3f06eac869a63795ed1
David Reynolds <davereynolds@outlook.com>
2020-03-20 16:44:10 +0000

    Added authentication

--- src/Notes.Web/src/index.tsx
+++ src/Notes.Web/src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from './contexts/auth0-context';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Auth0Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Auth0Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
